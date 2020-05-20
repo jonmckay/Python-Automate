184f56cfe52e69d8ef7d1c6ad83ec292ec02404b
jonmckay <jmckay0411@gmail.com>
2020-05-20 15:11:36 -0600

    Create a program to count characters in a string using a dictionary

---
+++ Section-7-Dictionaries/characterCount.py
@@ -0,0 +1,11 @@
+import pprint
+# Program to count the number of times each character appears in a string. This is done by saving to a dictionary
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
