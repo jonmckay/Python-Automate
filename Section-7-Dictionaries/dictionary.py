cf90cf97bee494a02fd56e57af411d1ff827a7d8
jonmckay <jmckay0411@gmail.com>
2020-05-20 14:44:32 -0600

    Use 'in' and 'not in' to find values in a dictionary

--- Section-7-Dictionaries/dictionary.py
+++ Section-7-Dictionaries/dictionary.py
@@ -3,6 +3,13 @@ spam = {12345: 'Luggage combination', 42: 'The Answer'}
 eggs = {'name' : 'Zophie', 'species': 'cat', 'age': 8}
 
 # Trying to find a key that doesn't exist in a dictionary will resule in a KeyError
-print(eggs['color'])
+# print(eggs['color'])
+
+# Use 'in' and 'not in' to search for values inside a dictionary
+print('name' in eggs)
+
+print('name' not in eggs)
+
+
 
     
