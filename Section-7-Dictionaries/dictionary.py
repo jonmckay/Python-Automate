5555c5970fa7da8ecc522fa68b290109a81d1a08
jonmckay <jmckay0411@gmail.com>
2020-05-20 14:41:00 -0600

    Show example of error received when requesting nonexistant value in dict

---
+++ Section-7-Dictionaries/dictionary.py
@@ -0,0 +1,8 @@
+spam = {12345: 'Luggage combination', 42: 'The Answer'}
+
+eggs = {'name' : 'Zophie', 'species': 'cat', 'age': 8}
+
+# Trying to find a key that doesn't exist in a dictionary will resule in a KeyError
+print(eggs['color'])
+
+    
